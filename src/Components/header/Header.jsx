1cdb2a03d0a9b4ec177cc8ed543771343bbb8988
Maksim Bekis <maxamax997@gmail.com>
2023-09-21 21:08:44 +0300

    add part menu

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,83 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { motion } from "framer-motion";
 import styled from "styled-components";
 
-const Wrapper = styled.header`
-  padding-top: 15px;
+function Header() {
+  const [active, setActive] = useState(true);
+  const Wrapper = styled.header`
+    padding-top: 15px;
 
-  div {
-    nav {
-      display: flex;
-      justify-content: space-between;
-      gap: 15px;
-      li {
-        list-style-type: none;
-        text-align: end;
-        a {
-          font-size: 1.2rem;
-          color: #828282;
-          font-family: "Gilroy-Bold";
-          line-height: 22.28px;
-          text-decoration: none;
-          white-space: nowrap;
-        }
-      }
-    }
-
-    @media all and (max-width: 600px) {
-      background: linear-gradient(
-        90deg,
-        rgba(255, 255, 255, 0) 0%,
-        rgb(187 191 194 / 35%) 10%,
-        rgb(141 149 153 / 73%) 23%,
-        rgb(89 101 107 / 78%) 39%,
-        rgb(26 43 51 / 81%) 64%,
-        rgb(26 43 51 / 98%) 90%,
-        rgba(26, 43, 51, 1) 100%
-      );
-      display: flex;
-      justify-content: space-between;
-      box-shadow: 20px 0 0 2vmax rgba(26, 43, 51, 1);
-      clip-path: inset(0-100vmax);
-
+    div {
       nav {
-        flex-direction: column;
-        align-items: end;
-        gap: 20px;
-      }
-      div {
-        background: rgba(255, 0, 0, 0);
-        box-shadow: 20px 0 0 0vmax rgba(255, 0, 0, 0);
-        width: 30px;
-        height: 30px;
-        position: relative;
-        z-index: 2;
-        overflow: hidden;
-
-        span {
-          width: 30px;
-          height: 2px;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          background-color: #222222;
-          transition: all 0.5s;
-        }
-
-        span:nth-of-type(2) {
-          top: calc(50% - 5px);
-        }
-        span:nth-of-type(3) {
-          top: calc(50% + 5px);
+        justify-content: space-between;
+        gap: 15px;
+        li {
+          list-style-type: none;
+          text-align: end;
+          a {
+            font-size: 1.2rem;
+            color: #828282;
+            font-family: "Gilroy-Bold";
+            line-height: 22.28px;
+            text-decoration: none;
+            white-space: nowrap;
+          }
         }
       }
     }
-  }
-`;
+    @media all and (max-width: 600px) {
+      padding-top: 0;
+      div {
+        height: 100vh;
+        background: ${active
+          ? "linear-gradient(90deg,rgba(255, 255, 255, 0) 0%,rgb(187 191 194 / 35%) 10%,rgb(141 149 153 / 73%) 23%,rgb(89 101 107 / 78%) 39%,rgb(26 43 51 / 81%) 64%,rgb(26 43 51 / 98%) 90%,rgba(26, 43, 51, 1) 100%)"
+          : "none"};
+        /* display: ${active ? "flex" : "none"}; */
+        justify-content: space-between;
+        box-shadow: ${active ? "20px 0 0 2vmax rgba(26, 43, 51, 1)" : "none"};
 
-const Header = function () {
+        clip-path: inset(0-100vmax);
+
+        nav {
+          display: ${active ? "flex" : "none"};
+          flex-direction: column;
+          align-items: end;
+          gap: 0px;
+          height: 90%;
+
+          a {
+            font-size: 6rem !important;
+          }
+        }
+        div {
+          background: rgba(255, 0, 0, 0);
+          box-shadow: ${active ? "20px 0 0 0vmax rgba(255, 0, 0, 0)" : "none"};
+          width: 50px;
+          height: 50px;
+          position: relative;
+          z-index: 2;
+          overflow: hidden;
+
+          span {
+            width: 50px;
+            height: 4px;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: #222222;
+            transition: all 0.5s;
+          }
+
+          span:nth-of-type(2) {
+            top: calc(50% - 15px);
+          }
+          span:nth-of-type(3) {
+            top: calc(50% + 15px);
+          }
+        }
+      }
+    }
+  `;
   const headerTitle = [
     {
       value: "Home",
@@ -118,7 +120,7 @@ const Header = function () {
   return (
     <Wrapper>
       <motion.div>
-        <div id="burgerButton" >
+        <div onClick={() => setActive(!active)} id="burgerButton">
           <span></span>
           <span></span>
           <span></span>
@@ -139,5 +141,5 @@ const Header = function () {
       </motion.div>
     </Wrapper>
   );
-};
+}
 export default Header;
